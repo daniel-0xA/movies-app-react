a61776127e228b8224d7aac37bf0b01a237d7838
Daniel Sporis <daniel@sporis.info>
2021-04-08 10:48:26 +0200

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState, useRef} from 'react'
 import Movie from './components/Movie'
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&vote_count.gte=1500&page=";
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&query=";
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&vote_count.gte=1500&api_key=2fa531bf8e1b0973e905bc082ef00707&page=";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=2fa531bf8e1b0973e905bc082ef00707&query=";
 
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [totalPages, setTotalPages] = useState(0)
   const inputEl = useRef(null)
 
   useEffect(() => {
@@ -17,8 +19,11 @@ const App = () => {
     fetch(API).then((response) => {
       return response.json()
     }).then((data) => {
-      console.log(data.results)
-      setMovies(data.results)
+      // console.log(data.results)
+      if(data.results){
+        setMovies(data.results)
+        setTotalPages(data.total_pages)
+      }
     })
   }
 
@@ -30,14 +35,34 @@ const App = () => {
       inputEl.current.value = ""
     }
   }
-  const goHomeHandler = (event) => {
-    fetchData(FEATURED_API)
+  const goHomeHandler = () => {
+    setCurrentPage(1)
+    fetchData(FEATURED_API + "1")
+  }
+  const goPrevHandler = () => {
+    if(currentPage > 1){
+      const page = currentPage - 1
+      setCurrentPage(page)
+      fetchData(FEATURED_API + page)
+    }
+  }
+  const goNextHandler = () => {
+    if(currentPage < totalPages){
+      const page = currentPage + 1
+      setCurrentPage(page)
+      fetchData(FEATURED_API + page)
+    }
   }
   
   return (
     <>
     <div className="header">
-      <button onClick={goHomeHandler}>Home</button>
+      <div className="header-pagination">
+        <button onClick={goHomeHandler}>Home</button>
+        <button onClick={goPrevHandler}>Prev</button>
+        <button onClick={goNextHandler}>Next</button>
+        <button className="button-noaction">Page: {currentPage}</button>
+      </div>
       <form onSubmit={onSubmitHandler}>
         <input className="header-search" type="search" placeholder="Search" ref={inputEl}/>
       </form>
